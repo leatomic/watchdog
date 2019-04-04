e98f260c8850460e31ee3cc4ab35d44beffccb77
leatomic <le.atomic@outlook.com>
2019-04-05 05:56:21 +0800

    remove /samples/provider-user_center/out/* and /samples/provider-user_center/build/*, then update .gitignore to ignore **/out/,**/build/

--- samples/provider-user_center/out/production/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into member_accounts(username, phone, password, gender, registration_time)
-  values  ('admin',       NULL,           '$2a$10$DBxgnk6GR5pwQRSgRpSy5eozq./Sl5Alc27ZKET0VY9INcAK8l5Ki', 'PRIVATE', CURRENT_DATE),
-          ('user',        NULL,           '$2a$10$NHOKSCBDy2rZ1tDDPIAdkOQxo.d.XCi8yokC0waoMi2XvVCe4FWGW', 'PRIVATE', CURRENT_DATE),
-          ('13570666666', '13570666666',  '$2a$10$uN1xKgb9JMBc/9qIaXilseMYtAYW5EwFvirmxfJxt7wLQ7wZpOE.i', 'PRIVATE', CURRENT_DATE);
